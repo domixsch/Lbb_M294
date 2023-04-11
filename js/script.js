66e986c5e97368cade43840e1304de2f994c356d
Dominik Schweizer <dominikschweizer06@gmail.com>
2023-04-11 16:26:10 +0200

    add und getTask

--- js/script.js
+++ js/script.js
@@ -1,17 +1,45 @@
-let addToDoButton = document.getElementById(`addToDo`);
+let addToDoButton = document.getElementById(`addTask`);
 let toDoContainer = document.getElementById(`toDoContainer`);
 let inputField = document.getElementById(`inputField`);
 
-addToDoButton.addEventListener(`click`, function(){
-    const paragraph  = document.createElement('p')
-    paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
-    inputField.value = "";
-    paragraph.addEventListener('click', function(){
-        paragraph.style.textDecoration = "line-through";
-    })
-    paragraph.addEventListener('dblclick', function(){
-        toDoContainer.removeChild(paragraph);
-    })
-})
+addToDoButton.addEventListener(`click`, function () {
+  const listElement = document.createElement("li");
+  listElement.classList.add("paragraph-styling");
+  listElement.innerText = inputField.value;
+  toDoContainer.appendChild(listElement);
+  inputField.value = "";
+  listElement.addEventListener("click", function () {
+  listElement.style.textDecoration = "line-through";
+  });
+  listElement.addEventListener("dblclick", function () {
+    toDoContainer.removeChild(listElement);
+  });
+});
+
+
+const tasksElement = document.getElementById("getTask");
+async function getTasks() {
+    const response = await fetch("http://localhost:3000/tasks", {
+        method: "GET"
+    });
+    const json = await response.json();
+
+    renderTasks(json);
+}
+
+async function addTask() {
+    const title = inputField.value;
+
+    const response = await fetch("http://localhost:3000/tasks", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+        },
+        body: JSON.stringify({
+            title: title,
+        }),
+    });
+
+    getTasks();
+}
+
