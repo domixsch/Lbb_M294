8bf026396792847d3d45832894540c5d8cbfba08
Dominik Schweizer <dominikschweizer06@gmail.com>
2023-04-12 09:50:25 +0200

    linethrough added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const tasksElement = document.getElementById("tasks");
 const inputField = document.getElementById(`inputField`);
 
-
 async function getTasks() {
   const response = await fetch("http://localhost:3000/tasks", {
     method: "GET",
@@ -12,21 +11,20 @@ async function getTasks() {
 }
 
 async function addTask() {
-    const title = inputField.value;
+  const title = inputField.value;
 
-    const response = await fetch("http://localhost:3000/tasks", {
-        method: "POST",
-        headers: {
-            "content-type": "application/json",
-        },
-        body: JSON.stringify({
-            title: title,
-        }),
-    });
+  const response = await fetch("http://localhost:3000/tasks", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      title: title,
+    }),
+  });
 
-    getTasks();
+  getTasks();
 }
-
 
 async function editTask(id) {
   const title = prompt("wie ");
@@ -59,8 +57,6 @@ async function deleteTask(id) {
   getTasks();
 }
 
-
-
 function renderTasks(tasks) {
   tasksElement.replaceChildren();
 
@@ -68,14 +64,39 @@ function renderTasks(tasks) {
     const liElement = document.createElement("li");
     liElement.innerText = item.title;
 
+    //wen man auf den Text drauf klickt wird eine linie generiert, falls es schon eine linie hat, wird sie weggenommen
+    liElement.addEventListener("mouseover", function (event) {
+      if (liElement.tagName === "LI") {
+        liElement.style.cursor = "pointer";
+      }
+    });
+
+    liElement.addEventListener("mouseout", function (event) {
+      if (liElement.tagName === "LI") {
+        liElement.style.cursor = "default";
+      }
+    });
+
+    liElement.addEventListener("click", function (event) {
+      if (liElement.tagName === "LI") {
+        if (liElement.style.textDecoration === "line-through") {
+          liElement.style.textDecoration = "none";
+        } else {
+          liElement.style.textDecoration = "line-through";
+        }
+      }
+    });
+
     const deleteButton = document.createElement("button");
-    deleteButton.innerHTML = '<img class="img" src="/images/delete.png" alt="Edit">';
+    deleteButton.innerHTML =
+      '<img class="img" src="/images/delete.png" alt="Edit">';
     deleteButton.onclick = function () {
       deleteTask(item.id);
     };
 
     const editButton = document.createElement("button");
-    editButton.innerHTML = '<img class="img" src="/images/edit.png" alt="Edit">';
+    editButton.innerHTML =
+      '<img class="img" src="/images/edit.png" alt="Edit">';
     editButton.onclick = function () {
       editTask(item.id);
     };
