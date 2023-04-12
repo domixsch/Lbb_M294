2de2b6365f4502ed5b7e3c283afadd024332c880
Dominik Schweizer <dominikschweizer06@gmail.com>
2023-04-12 08:59:44 +0200

    add,get,delete und editTask funktionieren

--- js/script.js
+++ js/script.js
@@ -1,30 +1,14 @@
-let addToDoButton = document.getElementById(`addTask`);
-let toDoContainer = document.getElementById(`toDoContainer`);
-let inputField = document.getElementById(`inputField`);
-
-addToDoButton.addEventListener(`click`, function () {
-  const listElement = document.createElement("li");
-  listElement.classList.add("paragraph-styling");
-  listElement.innerText = inputField.value;
-  toDoContainer.appendChild(listElement);
-  inputField.value = "";
-  listElement.addEventListener("click", function () {
-  listElement.style.textDecoration = "line-through";
-  });
-  listElement.addEventListener("dblclick", function () {
-    toDoContainer.removeChild(listElement);
-  });
-});
+const tasksElement = document.getElementById("tasks");
+const inputField = document.getElementById(`inputField`);
 
 
-const tasksElement = document.getElementById("getTask");
 async function getTasks() {
-    const response = await fetch("http://localhost:3000/tasks", {
-        method: "GET"
-    });
-    const json = await response.json();
+  const response = await fetch("http://localhost:3000/tasks", {
+    method: "GET",
+  });
+  const json = await response.json();
 
-    renderTasks(json);
+  renderTasks(json);
 }
 
 async function addTask() {
@@ -43,35 +27,59 @@ async function addTask() {
     getTasks();
 }
 
-async function deleteTask(id) {
-    const response = await fetch(`http://localhost:3000/tasks/${id}`, {
-        method: "DELETE",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify({
-            id: id
-        })
-    });
 
-    getTasks();
+async function editTask(id) {
+  const title = prompt("wie ");
+
+  const response = await fetch("http://localhost:3000/tasks", {
+    method: "PUT",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      id: id,
+      title: title,
+    }),
+  });
+
+  getTasks();
 }
 
+async function deleteTask(id) {
+  const response = await fetch(`http://localhost:3000/task/${id}`, {
+    method: "DELETE",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      id: id,
+    }),
+  });
+
+  getTasks();
+}
 
 function renderTasks(tasks) {
-    toDoContainer.replaceChildren();
+  tasksElement.replaceChildren();
 
-    tasks.forEach(function (item) {
-        const liElement = document.createElement("li");
-        liElement.innerText = item.title;
+  tasks.forEach(function (item) {
+    const liElement = document.createElement("li");
+    liElement.innerText = item.title;
 
-        const deleteButton = document.createElement("button");
-        deleteButton.innerText = "Delete";
-        deleteButton.onclick = function () {
-            deleteTask(item.id);
-        };
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.onclick = function () {
+      deleteTask(item.id);
+    };
 
-        liElement.append(deleteButton);
-        toDoContainer.append(liElement);
-    });
+    const editButton = document.createElement("button");
+    editButton.innerText = "Edit";
+    editButton.onclick = function () {
+      editTask(item.id);
+    };
+
+    liElement.append(deleteButton);
+    liElement.append(editButton);
+    tasksElement.append(liElement);
+  });
 }
